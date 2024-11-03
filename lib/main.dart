c135255e1b4794a30f7185842b7f8a6e611e5397
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2024-11-03 10:49:59 -0500

    Primer cambio

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:quizappg10/pages/home_page.dart';
 
+
+
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(),
+      home: HomePage(),   
       debugShowCheckedModeBanner: false,
     ),
   );
